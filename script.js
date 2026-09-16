b87413739442181e9ebb5caedf7d8de33d26a0e2
lFelipeSilval <lufelipe4594@gmail.com>
2026-09-16 14:46:42 -0300

    Adicionado novas moedas e corrigido alguns bugs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
 function convertValues() {
     
@@ -9,20 +10,84 @@ function convertValues() {
     const currencyValueToConverted = document.querySelector(".currency-value") // Valor convertido em outras moedas
 
     const dolarToday = 5.15
+    const euroToday = 5.94
+    const libraToday = 6.92
+    const bitToday = 390530.00
+    const ienetoday = 0.033
 
-    const convertedValue = inputCurrency / dolarToday
+    if (currencySelect.value == "Dolar") {
+    currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }) .format(inputCurrency / dolarToday)
+    }
+
+    if (currencySelect.value == "Euro") {
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }) .format(inputCurrency / euroToday)
+    }
+
+    if (currencySelect.value == "libra") {
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }) .format(inputCurrency / libraToday)
+    }
+
+    if (currencySelect.value == "bit") {
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC"
+        }) .format(inputCurrency / bitToday)
+    }
+
+    if (currencySelect.value == "iene") {
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("ja-JP", {
+            style: "currency",
+            currency: "JPY"
+        }) .format(inputCurrency / ienetoday)
+    }
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
 
         style: "currency",
         currency: "BRL"
     }) .format(inputCurrency)
+}
 
-    currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }) .format(convertedValue)
+
+function changeCurrency() {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImg = document.querySelector(".currency-img")
+
+    if(currencySelect.value == "Dolar") {
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImg.src = "./assets/estados-unidos (1) 1.png"
+    }
+
+    if(currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra Esterlina"
+        currencyImg.src = "./assets/libra.png"
+    }
+
+    if(currencySelect.value == "Euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./assets/euro.png"
+    }
+
+    if(currencySelect.value == "bit") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImg.src = "./assets/bitcoin.png"
+    }
+
+    if(currencySelect.value == "iene") {
+        currencyName.innerHTML = "Iene Japonês"
+    }
 
 }
 
+currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
+currencySelect.addEventListener("change", convertValues)
