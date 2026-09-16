0a668f1cc4d530468cefdd3c22e18e8950622691
lFelipeSilval <lufelipe4594@gmail.com>
2026-09-16 13:41:46 -0300

    Formatado números de conversão e adcionado nos devidos campos

--- script.js
+++ script.js
@@ -2,8 +2,27 @@
 const convertButton = document.querySelector(".convert-button")
 
 function convertValues() {
+    
     const inputCurrency = document.querySelector(".input-currency").value
-    console.log(inputCurrency)
+
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor em real que será convertido
+    const currencyValueToConverted = document.querySelector(".currency-value") // Valor convertido em outras moedas
+
+    const dolarToday = 5.15
+
+    const convertedValue = inputCurrency / dolarToday
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+
+        style: "currency",
+        currency: "BRL"
+    }) .format(inputCurrency)
+
+    currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }) .format(convertedValue)
+
 }
 
 convertButton.addEventListener("click", convertValues)
